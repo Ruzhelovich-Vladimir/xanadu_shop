2273d6b184598947fea26c62c0d486c5fd3b454a
OmskShip <Ruzhelovich.Vladimir@gmail.com>
2020-12-07 08:15:33 +0600

    lesson-2: template + content = html, remarks correction

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 product_simple = {
-    "title":"Bouble Fabric Blazer",
-    "img":"assets/images/products/featured/1.jpg",
-    "old_price":"700.00",
-    "price":"69.90",
-    "description":"Dramatically transition excellent information rather than mission-critical results. Competently communicate fully tested core competencies through holistic resources. Professionally maintain high-payoff best practices whereas user-centric alignments. Intrinsicly engage future-proof best practices whereas economically sound resources. Holisticly maximize multidisciplinary synergy before magnetice-tailers."
+    'title':'Bouble Fabric Blazer',
+    'img':'assets/images/products/featured/1.jpg',
+    'old_price':'700.00',
+    'price':'69.90',
+    'description':'Dramatically transition excellent information rather than mission-critical results. Competently communicate fully tested core competencies through holistic resources. Professionally maintain high-payoff best practices whereas user-centric alignments. Intrinsicly engage future-proof best practices whereas economically sound resources. Holisticly maximize multidisciplinary synergy before magnetice-tailers.'
 }
 
 product_simple_list = [product_simple for inx in range(15)]
@@ -13,7 +13,7 @@ product_simple_list = [product_simple for inx in range(15)]
 def main(request):
 
     content = {
-        "title": "main"
+        'title': 'main'
     }
 
     return render(request, 'mainapp/index.html',content)
@@ -21,11 +21,11 @@ def main(request):
 def catalog(request):
 
     content = {
-        "title": "catalog",
-        "products": product_simple_list,
-        "start":1,
-        "finish":15,
-        "count":15
+        'title': 'catalog',
+        'products': product_simple_list,
+        'start':1,
+        'finish':15,
+        'count':15
     }
 
     return render(request, 'mainapp/catalog.html',content)
@@ -33,7 +33,7 @@ def catalog(request):
 def about(request):
 
     content = {
-        "title": "about",
+        'title': 'about',
     }
 
     return render(request, 'mainapp/about.html',content)
