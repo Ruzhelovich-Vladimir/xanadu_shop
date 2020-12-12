5e1dc2b371ec6eb50c485f080fda2fdc2ed10ecd
OmskShip <Ruzhelovich.Vladimir@gmail.com>
2020-12-13 00:38:45 +0600

    lesson-3: Generate fixture, creation models, modify templates and styles

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Brand, Package, Classification, Country, Strong, Style, Product, PayForms, Warehouse, WarehouseStocks
 
-# Register your models here.
+admin.site.register(Brand)
+admin.site.register(Package)
+admin.site.register(Classification)
+admin.site.register(Country)
+admin.site.register(Strong)
+admin.site.register(Style)
+admin.site.register(Product)
+admin.site.register(Warehouse)
+admin.site.register(WarehouseStocks)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,9 @@
+from django.conf import settings
 from django.shortcuts import render
+from django.utils import timezone
 
-product_simple = {
-    'title':'Bouble Fabric Blazer',
-    'img':'assets/images/products/featured/1.jpg',
-    'old_price':'700.00',
-    'price':'69.90',
-    'description':'Dramatically transition excellent information rather than mission-critical results. Competently communicate fully tested core competencies through holistic resources. Professionally maintain high-payoff best practices whereas user-centric alignments. Intrinsicly engage future-proof best practices whereas economically sound resources. Holisticly maximize multidisciplinary synergy before magnetice-tailers.'
-}
+from .models import Brand, Package, Classification, Country, Strong, Style, Product
 
-product_simple_list = [product_simple for inx in range(15)]
 
 def main(request):
 
@@ -16,19 +11,27 @@ def main(request):
         'title': 'main'
     }
 
-    return render(request, 'mainapp/index.html',content)
+    return render(request, 'mainapp/index.html', content)
+
 
 def catalog(request):
 
+    start = 1
+    finish = 15
+    count = 15
+
+    products = Product.objects.all()[start-1:finish-1]
+
     content = {
         'title': 'catalog',
-        'products': product_simple_list,
-        'start':1,
-        'finish':15,
-        'count':15
+        'products': products,
+        'start': start,
+        'finish': finish,
+        'count': count
     }
 
-    return render(request, 'mainapp/catalog.html',content)
+    return render(request, 'mainapp/catalog.html', content)
+
 
 def about(request):
 
@@ -36,5 +39,4 @@ def about(request):
         'title': 'about',
     }
 
-    return render(request, 'mainapp/about.html',content)
-
+    return render(request, 'mainapp/about.html', content)
